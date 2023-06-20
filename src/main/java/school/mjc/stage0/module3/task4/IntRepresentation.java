4a989834af419a6225ca026a87a5cb89e7e68126
Nursultan <nkulmambetov@bk.ru>
2023-06-20 23:59:15 +0300

    Make task

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
     }
 }
